19d7f4decdd50188869bba75a7b2fd8eb838b227
PoojaSridharan05 <89025672+PoojaSridharan05@users.noreply.github.com>
2024-01-14 23:30:26 +0530

    TodoService.java

--- src/main/java/com/in28minutes/todo/service/TodoService.java
+++ src/main/java/com/in28minutes/todo/service/TodoService.java
@@ -1,4 +1,4 @@
-package com.in28minutes.todo.service;
+package com.in.todo.service;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -7,7 +7,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import com.in28minutes.model.Todo;
+import com.in.model.Todo;
 
 @Service
 public class TodoService {
